266221668bb03af878cf2b914a53db2c21ec27e1
RomanDribble <romandribble@outlook.com>
2018-05-21 09:54:01 -0400

    Update

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :comments, dependent: :destroy
   has_many :shots, dependent: :destroy
+
+  acts_as_voter
 end
